6bf8df89ddd691865f78c56ea339dcec1a714e79
Yeji Yang <yyj_a95@naver.com>
2023-09-18 16:49:07 +0900

    2st commit

---
+++ 기본 Setting.sh
@@ -0,0 +1,35 @@
+# 기본 Setting
+sudo apt install git
+sudo apt install make
+sudo apt install gcc
+sudo apt install cmake
+sudo apt install g++
+
+# WiringPi install
+git clone https://github.com/WiringPi/WiringPi.git
+cd WiringPi
+./build
+gpio -v
+
+# I2C Setting
+lsmod | grep i2c
+sudo modprobe i2c-dev
+sudo usermod -aG i2c 사용자_이름
+sudo raspi-config
+->interface option ->i2c ->ok
+
+# cFS install
+git clone https://github.com/nasa/cFS.git
+cd cFS
+git submodule init
+git submodule update
+
+cp cfe/cmake/Makefile.sample Makefile
+cp -r cfe/cmake/sample_defs sample_defs
+
+make SIMULATION=native prep
+make
+make install
+cd build/exe/cpu1/
+./core-cpu1
+
